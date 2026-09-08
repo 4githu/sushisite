511842cf513d4d3cb86abc4e752f5a04ff5ae360
boracles <100pour100@naver.com>
2026-09-08 18:26:12 +0900

    Allow distinct YuJin voice for third female audience

--- sushi-fast/odi/EVC/azure_speech.py
+++ sushi-fast/odi/EVC/azure_speech.py
@@ -70,7 +70,7 @@ async def _transcribe(data):
 
 
 VOICES = ("ko-KR-InJoonNeural", "ko-KR-BongJinNeural", "ko-KR-GookMinNeural",
-          "ko-KR-SunHiNeural", "ko-KR-JiMinNeural", "ko-KR-SeoHyeonNeural", "ko-KR-SoonBokNeural")
+          "ko-KR-SunHiNeural", "ko-KR-JiMinNeural", "ko-KR-SeoHyeonNeural", "ko-KR-SoonBokNeural", "ko-KR-YuJinNeural")
 
 
 async def synthesize(text, voice):
